828c8e1bb26f3b9caa4a127a85934e647f6c15d7
Dummyc0m <m.steve.jobs@me.com>
2017-03-30 21:29:55 -0400

    directly encode jsonobj

--- src/main/kotlin/cn/com/guardiantech/classroom/backend/util/json.kt
+++ src/main/kotlin/cn/com/guardiantech/classroom/backend/util/json.kt
@@ -11,6 +11,9 @@ import cn.codetector.jet.jetsimplejson.JSONObject
 fun jsonObjectOf(vararg fields: Pair<String, Any?>) = JSONObject(mapOf(*fields))
 fun jsonArrayOf(vararg values: Any?) = JSONArray(listOf(*values))
 
+fun encodedJsonObjectOf(vararg fields: Pair<String, Any?>) = JSONObject(mapOf(*fields)).encode()
+fun encodedJsonArrayOf(vararg values: Any?) = JSONArray(listOf(*values)).encode()
+
 @Suppress("UNCHECKED_CAST")
 fun <T> JSONObject.get(k: String) = getValue(k) as T
 
